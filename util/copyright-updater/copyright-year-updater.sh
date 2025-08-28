71eed929d1835169e7cf08db2748e80f7a1d589a
Kris Hall <kjh@ieee.org>
2025-08-28 05:55:25 -0400

    cleanup license statements

--- util/copyright-updater/copyright-year-updater.sh
+++ util/copyright-updater/copyright-year-updater.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-# (c) Copyright 2024-2025 'exec' Contributors. All rights reserved.
+# (c) Copyright 2024-2025 exec Contributors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
